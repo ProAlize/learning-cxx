f3a86a5f604c1fdc4daf47e411a96c05f47cb236
ProAlize <1317460037@qq.com>
2024-12-27 21:26:20 +0800

    cpp7

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -5,10 +5,12 @@
 // READ: 纯函数 <https://zh.wikipedia.org/wiki/%E7%BA%AF%E5%87%BD%E6%95%B0>
 static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
-    static unsigned long long cache[96], cached;
+    static unsigned long long cache[96] = {0,1};
     // TODO: 设置正确的循环条件
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    for (int j = 2; j<=i ; j++) {
+        if(cache[j]==0){
+        cache[j] = cache[j - 1] + cache[j - 2];
+        }
     }
     return cache[i];
 }
