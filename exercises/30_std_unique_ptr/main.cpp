b2be247abcc3bd7a8e480c2dc7859319a1a97b0a
ProAlize <1317460037@qq.com>
2025-01-02 18:47:59 +0800

    cpp30

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -50,9 +50,11 @@ int main(int argc, char **argv) {
     // ---- 不要修改以上代码 ----
 
     std::vector<const char *> answers[]{
-        {"fr", "d"}, // problems[0]
-        {"fr", "f", "fr", "d", "f"}, // problems[1]
-        {"r", "r", "d", "r", "d", "d"} // problems[2]
+        {"fd"},
+        // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
+        // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
+        {"ffr", "d"},
+        {"r", "d", "d"},
     };
 
     // ---- 不要修改以下代码 ----
