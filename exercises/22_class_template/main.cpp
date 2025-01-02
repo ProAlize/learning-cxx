64b2beab8dc2713006689d9c7792aef756ff13f7
ProAlize <1317460037@qq.com>
2025-01-02 14:29:46 +0800

    cpp22

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,4 +1,6 @@
 ﻿#include "../exercise.h"
+#include <cstring>
+#include <stdexcept>
 
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
@@ -9,7 +11,11 @@ struct Tensor4D {
 
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
+        // 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -27,7 +33,56 @@ struct Tensor4D {
     // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
-        // TODO: 实现单向广播的加法
+        // 检查形状是否兼容
+        for (int i = 0; i < 4; ++i) {
+            if (others.shape[i] != 1 && others.shape[i] != this->shape[i]) {
+                throw std::invalid_argument("Shapes are not compatible for broadcasting.");
+            }
+        }
+
+        // 计算 this 的 strides
+        unsigned int this_strides[4];
+        this_strides[3] = 1;
+        for (int i = 2; i >= 0; --i) {
+            this_strides[i] = this_strides[i + 1] * this->shape[i + 1];
+        }
+
+        // 计算 others 的 strides，若某维度为 1 则 stride 为 0
+        unsigned int others_strides[4];
+        unsigned int current_stride = 1;
+        for (int i = 3; i >= 0; --i) {
+            if (others.shape[i] == 1) {
+                others_strides[i] = 0;
+            } else {
+                others_strides[i] = current_stride;
+                current_stride *= others.shape[i];
+            }
+        }
+
+        unsigned int total_size = 1;
+        for (int i = 0; i < 4; ++i) {
+            total_size *= this->shape[i];
+        }
+
+        for (unsigned int idx = 0; idx < total_size; ++idx) {
+            // 计算 multi-dimensional index
+            unsigned int remaining = idx;
+            unsigned int indices[4];
+            for (int i = 0; i < 4; ++i) {
+                indices[i] = remaining / this_strides[i];
+                remaining %= this_strides[i];
+            }
+
+            // 计算 others 的线性索引
+            unsigned int others_idx = 0;
+            for (int i = 0; i < 4; ++i) {
+                others_idx += indices[i] * others_strides[i];
+            }
+
+            // 执行加法
+            this->data[idx] += others.data[others_idx];
+        }
+
         return *this;
     }
 };
